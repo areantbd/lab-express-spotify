bbbc0ae0ba723c32e77e1b5186aec06241d52de7
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-06-27 21:45:06 +0200

    home done

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,1 +1,12 @@
 // TODO
+const express = require('express');
+const router = express.Router();
+const misc = require('../controllers/misc.controller');
+const spotify = require('../controllers/spotify.controller')
+
+
+router.get('/', misc.home);
+router.get('/artist-search', spotify)
+
+
+module.exports = router;
--- controllers/spotify.controller.js
+++ controllers/spotify.controller.js
@@ -1,1 +1,19 @@
 // TODO
+const SpotifyWebApi = require('spotify-web-api-node')
+
+module.exports.doSearch = (req, res, next) => {
+    res.render('vista')
+
+}
+module.exports.doSearchArtists = (req, res, next) => {
+spotifyApi
+  .searchArtists(req.query.search)
+  .then((data) => {
+    console.log("The received data from the API: ", data.body);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    res.render('/', {artists : data.body.artists.items})
+})
+  .catch((err) =>
+    console.log("The error while searching artists occurred: ", err)
+  );
+}
